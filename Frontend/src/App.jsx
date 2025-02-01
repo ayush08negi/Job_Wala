84eecff3fdf74079f623104fa0953942e7d58f23
ayush08negi <negi08ayush@gmail.com>
2025-02-01 19:26:43 +0530

    frontend started after this

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
+ 
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   <h1 className='bg-red-500'> HHIII </h1>
-   </>
+    <>
+     <Button>Click me</Button>
+    </>
   )
 }
 
